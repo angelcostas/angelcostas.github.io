209a52e8723979bde3b5cb14b0553eb81e95fe9d
Angel Costas <54624583+angelcostas@users.noreply.github.com>
2019-08-31 16:34:55 +0200

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -7,7 +7,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
     dotColor: '#5cbdaa',
-    lineColor: '#000000'
+    lineColor: '#5cbdaa'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
